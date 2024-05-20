b09ef77644570b81bb88474cd44f03b217fc1acf
Archana-Shailvee-Aaditya <kaushal@Archana-shailvee-aadityas-MacBook-Pro.local>
2024-05-20 17:54:55 -0400

    initial v

---
+++ src/test/java/com/kfinatech/security/saml2/Kfinatech/security/ample/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfinatech.security.saml2.Kfinatech.security.ample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
